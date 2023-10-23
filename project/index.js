f97920e50d7dfde6d69a491feb92278d87c4b29a
Liliia Sharhorodska <lilya.osipchuk@gmail.com>
2023-10-23 18:38:15 +0300

    first commit

---
+++ project/index.js
@@ -0,0 +1,9 @@
+import { a1Value } from "./module-a/a1.js";
+import { a2Value } from "./module-a/a2.cjs";
+import { b1Value } from "./module-b/b1.js";
+import { b2Value } from "./module-b/b2.mjs";
+
+console.log(a1Value);
+console.log(a2Value);
+console.log(b1Value);
+console.log(b2Value);
